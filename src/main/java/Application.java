d376faf228610629c441f8be328b176f9507c855
Home <ildar86@live.ru>
2023-02-13 17:59:51 +0500

    задание 1 выполнено. создан класс Application, установлено соединение с базой данных, вывел в консоль данные об одном из работников

---
+++ src/main/java/Application.java
@@ -0,0 +1,39 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static java.sql.DriverManager.getConnection;
+
+public class Application {
+    public static void main(String[] args) throws SQLException {
+        final String user = "postgres";
+        final String password = "1902";
+        final String url = "jdbc:postgresql://localhost:5432/skypro";
+
+        try (final Connection connection = getConnection(url, user, password);
+             PreparedStatement statement = connection.prepareStatement(
+                     "select name, surname, gender, age, city_name " +
+                             "from employee inner join city " +
+                             "on city.city_id=employee.city_id " +
+                             "where employee.id = (?);")) {
+            statement.setInt(1, 4);
+            final ResultSet resultSet = statement.executeQuery();
+            System.out.println("---------------------");
+            while (resultSet.next()) {
+                String name = "Имя: " + resultSet.getString("name");
+                String surname = "Фамилия: " + resultSet.getString("surname");
+                String cityName = "Город проживания: " + resultSet.getString("city_name");
+                String gender = "Пол: " + resultSet.getString("gender");
+                int age = resultSet.getInt("age");
+
+                System.out.println(name );
+                System.out.println(surname );
+                System.out.println(cityName );
+                System.out.println(gender );
+                System.out.println("Возраст: " + age );
+                System.out.println("---------------------");
+            }
+        }
+    }
+}
